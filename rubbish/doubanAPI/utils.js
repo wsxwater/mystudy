8e7e20e946c9c05e03ddd04d7f73ace45b4743d8
wsxwater <675162796@qq.com>
2018-05-09 11:30:41 +0800

    add my-case/doubanAPI

---
+++ rubbish/doubanAPI/utils.js
@@ -0,0 +1,36 @@
+function httpGet(url,callback) {
+	$.ajax({
+		url:url,
+		type:'GET',
+		dataType: 'jsonp',
+        crossDomain: true,
+		success:function (res) {
+			callback(res)
+		},
+		error:function (res) {
+			alert('error')
+		},
+	})
+}
+
+
+function getUrlParam(name) {
+    var reg=new RegExp("(^|&)"+name+"=([^&]*)(&|$)");
+    var r=window.location.search.substr(1).match(reg);
+
+    if (r!=null) {
+        return unescape(r[2]);
+        return null;
+    }
+}
+
+
+function getChinese(strValue) {  
+    if(strValue!= null && strValue!= ""){  
+        var reg = /[\u4e00-\u9fa5]/g;   
+	    var result = strValue.match(reg).join("");  
+	    return result; 
+    }else { 
+        return ""; 
+    } 
+}  
