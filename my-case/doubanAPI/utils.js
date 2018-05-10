e786386522f54f8ec0515de37c76fb3e21866d76
wsxwater <675162796@qq.com>
2018-05-10 18:45:41 +0800

    modify code

--- my-case/doubanAPI/utils.js
+++ my-case/doubanAPI/utils.js
@@ -1,17 +1,52 @@
-function httpGet(url,callback) {
-	$.ajax({
-		url:url,
-		type:'GET',
-		dataType: 'jsonp',
-        crossDomain: true,
-		success:function (res) {
-			callback(res)
-		},
-		error:function (res) {
-			alert('error');
-			window.location.href='list.html';
-		},
-	})
+var gobalurl='https://api.douban.com';
+
+
+function httpGet(url,callback,error) {
+	var xhrs=$.ajax({
+			url:url,
+			type:'GET',
+			dataType: 'jsonp',
+	        crossDomain: true,
+			success:function (res) {
+				callback(res)
+			},
+			error:function (res) {
+				//alert('error');
+				//window.location.href='list.html';
+				//$('.tip').text('加载失败。。。');
+				error();
+			},
+		});
+}
+
+function starToArr(stars){
+    var arr=[];
+    var num=stars.substring(0,1);
+
+    for(var i=1;i<=5;i++){
+      if(num<i){
+        arr.push(0);
+      }else{
+        arr.push(1)
+      }
+    }
+
+    return arr;
+}
+
+function appendStar(star,parent) {
+    for (var i = 0; i < star.length; i++) {
+        var starArr=[];
+        var starImg='<span>★</span>';
+
+        if (star[i]==1) {
+            starImg='<span class="on">★</span>';
+        }else if (star[i]==0) {
+            starImg='<span>★</span>';
+        }
+        starArr.push(starImg);
+        parent.append(starArr);
+    }
 }
 
 
