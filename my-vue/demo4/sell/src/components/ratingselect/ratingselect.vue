eebdcf4b4db694b325cf8bc9c02b60effa894b82
wsxwater <675162796@qq.com>
2019-10-02 15:54:59 +0800

    modifyied:ratingselect.vue

--- my-vue/demo4/sell/src/components/ratingselect/ratingselect.vue
+++ my-vue/demo4/sell/src/components/ratingselect/ratingselect.vue
@@ -11,8 +11,8 @@
     </div>
   </div>
 </template>
-<!--  type="text/ecmascript-6" -->
-<script>
+
+<script type="text/ecmascript-6">
   // const POSITIVE = 0;
   // const NEGATIVE = 1;
   const ALL = 2;
