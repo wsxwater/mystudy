dc4efe23eb0f8c6114539bed851dfdf57d55f416
wsxwater <675162796@qq.com>
2019-10-20 23:08:07 +0800

    modify date.js and modify .eslintrc.js

--- my-vue/demo4/sell/src/common/js/date.js
+++ my-vue/demo4/sell/src/common/js/date.js
@@ -1,4 +1,3 @@
-/* eslint-disable */
 export function formatDate (date, fmt) {
 	if (/(y+)/.test(fmt)) {
 		fmt = fmt.replace(RegExp.$1, (date.getFullYear() + '').substr(4 - RegExp.$1.length));
@@ -13,7 +12,7 @@ export function formatDate (date, fmt) {
 	for (let k in o) {
 		if (new RegExp(`(${k})`).test(fmt)) {
 			let str = o[k] + '';
-			fmt = fmt.replace(RegExp.$1,(RegExp.$1.length === 1) ? str : addZero(str));
+			fmt = fmt.replace(RegExp.$1, (RegExp.$1.length === 1) ? str : addZero(str));
 		}
 	}
 	return fmt;
